ec01af6b8c01f93c81262db9a62fce7ed3444acf
Robert de Vries <r.devries@robbytu.net>
2013-05-12 04:22:08 +0200

    Register ourselves as event listener

--- src/main/java/net/robbytu/banjoserver/tablist/Main.java
+++ src/main/java/net/robbytu/banjoserver/tablist/Main.java
@@ -1,10 +1,15 @@
 package net.robbytu.banjoserver.tablist;
 
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
+public class Main extends JavaPlugin implements Listener {
 	@Override
 	public void onEnable() {
+		// Register for events
+		getServer().getPluginManager().registerEvents(this, this);
+		
+		// We're done! Yay!
 		getLogger().info("Tablist has been enabled.");
 	}
 	
