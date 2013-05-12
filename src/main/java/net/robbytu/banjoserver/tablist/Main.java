752ae8badbf70ee470ea13cb74b836077c88163f
Robert de Vries <r.devries@robbytu.net>
2013-05-12 04:22:53 +0200

    Listen for PlayerJoin events

--- src/main/java/net/robbytu/banjoserver/tablist/Main.java
+++ src/main/java/net/robbytu/banjoserver/tablist/Main.java
@@ -1,6 +1,9 @@
 package net.robbytu.banjoserver.tablist;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
@@ -16,5 +19,11 @@ public class Main extends JavaPlugin implements Listener {
 	@Override
 	public void onDisable() {
 		getLogger().info("Tablist has been disabled.");
+	}
+	
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent event) {
+		final Player p = event.getPlayer();
+		// Todo
 	}
 }
